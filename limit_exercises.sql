859685a8ffcb899cc2edb74049609a442d48e983
John Nichols <jtn0113@yahoo.com>
2017-04-05 15:25:43 -0500

    updated limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,16 +5,14 @@ limit 10 offset 0;
 
 select * 
 from employees 
-where hire_date 
-between '1990-01-01' and '1999-12-31'
+where hire_date like '199%'
 and birth_date like '%-12-25'
 order by birth_date, hire_date DESC
 LIMIT 5 OFFSET 0;
 
 select * 
 from employees 
-where hire_date 
-between '1990-01-01' and '1999-12-31'
+where hire_date LIKE '199%'
 and birth_date like '%-12-25'
 order by birth_date, hire_date DESC
 LIMIT 5 OFFSET 45;
