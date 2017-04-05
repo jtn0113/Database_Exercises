6bb01f741dcffb572148c72c302e0373a83f0bbc
John Nichols <jtn0113@yahoo.com>
2017-04-05 11:20:36 -0500

    updated where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,25 @@
 
-select * from employees where first_name in ('Irena', 'Vidya', 'Maya');
+select * 
+from employees 
+where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
+and gender = 'M';
 
-select * from employees where last_name = (E%);
 
-select * from employees where hire_date between '1990-01-01' and '1999-12-31';
 
-select * from employees where birth_date like '%-12-25';
+select * 
+from employees 
+where last_name 
+like 'e%' and  last_name like '%e';
 
-select * from employees where last_name like '%q%';
+
+select * 
+from employees 
+where hire_date 
+between '1990-01-01' and '1999-12-31'
+and birth_date like '%-12-25';
+
+select * 
+from employees 
+where last_name 
+like '%q%'
+and last_name not like '%qu%';
