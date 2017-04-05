861263565ca5ee7a68c39f2b9c421244264a2854
John Nichols <jtn0113@yahoo.com>
2017-04-05 10:11:14 -0500

    added where exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,6 +1,6 @@
 USE codeup_test_db;
 
 delete from albums where release_date > '1991-01-01';
-delete from albums where genre = '%disco%';
+delete from albums where genre like '%disco%';
 delete from albums where artist_name = 'Eagles';
 
